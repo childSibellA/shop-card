d5b304f58d18b12a5c15bb48a63cfef06570f961
childSibellA <pipiaalex7@gmail.com>
2023-01-31 06:56:05 -0800

    heloo

--- src/components/ChoosenProducts/Choosen.js
+++ src/components/ChoosenProducts/Choosen.js
@@ -1,7 +1,29 @@
+import './Choosen.css'
+
 const Choosen = () => {
     return (
-        <div className="Choosen">
-            gaajvi
+        <div className='chosenBackground'>
+            <div className="Choosen">
+                <div className="choosenProducts">
+                    <div className='choosenImg'></div>
+                    <div className='nameAndPrice'>
+                        <p className='choosenName'>name</p>
+                        <div className='choosenPrice'>price</div>
+                        <div>amount:</div>
+                    </div>
+                    <div className='btns' >
+                    <div className='removeButton'>remove</div>
+                    <div className='addButton'>Add</div>
+                    </div>
+                </div>
+
+
+
+
+                <div>total price:</div>
+
+
+            </div>
         </div>
     );
 }
---
+++ src/components/Products/ProductList.js
@@ -0,0 +1,19 @@
+const PRODACTS_LIST = [
+    {
+        name: "hp230",
+        id: 1,
+        price: 300, 
+    }, 
+    {
+        name: "hp230",
+        id: 2,
+        price: 400, 
+    }, 
+    {
+        name: "hp230",
+        id: 3,
+        price: 500, 
+    }, 
+]
+
+export default PRODACTS_LIST;
